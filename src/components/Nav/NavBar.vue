e9b07349975323e94b684e3149dc07667ac84ba2
szt2otokov0 <szt2otokov@vasvari.org>
2024-11-10 20:39:29 +0100

    more navbar things

--- src/components/Nav/NavBar.vue
+++ src/components/Nav/NavBar.vue
@@ -10,7 +10,7 @@ import NavItem from "@/components/Nav/NavItem.vue";
   </div>
   <ul class="flex flex-row justify-around col-span-3 col-start-2">
     <li>
-      <NavItem title="Home" url="https://localhost:5173/" :options="[{title:'hello',url:'https://www.aaa.com'},
+      <NavItem title="Home" url="http://localhost:5173/" :options="[{title:'hello',url:'https://www.aaa.com'},
       {title: 'hello 2',url: 'https://service.neptun.u-szeged.hu'},{title: 'hello 3', url: 'https://www.alma.io'}]">
       </NavItem>
     </li>
--- src/components/Nav/NavItem.vue
+++ src/components/Nav/NavItem.vue
@@ -10,25 +10,31 @@ let isMenuOpen = ref(false)
 </script>
 
 <template>
-  <a id="title" :href="url">{{ itemName }}</a>
-  <div id="expand-arrow" v-if="hasSuboptions" @click="isMenuOpen = !isMenuOpen" class="cursor-pointer">V</div>
-  <Transition>
-    <div v-if="isMenuOpen" class="border-2 border-black">
-      <div id="submenu-dropdown" v-for="s in suboptions">
-        <a :href="s.url">{{ s.title }}</a>
-      </div>
+  <div class="flex flex-col justify-between w-max">
+    <div class="bg-white flex z-50">
+      <a class="relative" :href="url">{{ itemName }}</a>
+      <div v-if="hasSuboptions" @click="isMenuOpen = !isMenuOpen" @focus="isMenuOpen = true"
+           @blur="isMenuOpen = false" class="cursor-pointer ml-1">V</div>
     </div>
-  </Transition>
+    <Transition>
+      <div v-if="isMenuOpen" class="border border-gray-500 p-2 shadow-md relative z-10">
+        <div v-for="s in suboptions">
+          <a :href="s.url">{{ s.title }}</a>
+        </div>
+      </div>
+    </Transition>
+  </div>
 </template>
 
 <style scoped>
 .v-enter-active,
 .v-leave-active {
-  transition: all 0.1s ease;
+  transition: all 0.1s cubic-bezier(0, 0.55, 0.45, 1);
 }
 
 .v-enter-from,
 .v-leave-to {
-  transform: translateY(-50%);
+  transform: translateY(-20%);
+  box-shadow: inset 0 1px 0 gray;
 }
 </style>
