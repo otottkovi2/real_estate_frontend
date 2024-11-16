6d3ec06d742d3d842da6c910010e95c8968361ed
szt2otokov0 <szt2otokov@vasvari.org>
2024-11-16 16:25:28 +0100

    fixed Card slots done wrong

--- src/components/Layout/Card.vue
+++ src/components/Layout/Card.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="p-2 my-2 mx-auto rounded-md shadow-md border-2 border-gray-500 ">
-    <card-header v-if="$slots['card-header']">
-      <slot name="card-header"></slot>
-    </card-header>
-    <card-body>
-      <slot></slot>
-    </card-body>
-    <card-footer v-if="$slots['card-footer']">
-      <slot name="card-footer"></slot>
-    </card-footer>
+    <slot name="card-header"></slot>
+    <slot></slot>
+    <slot name="card-footer"></slot>
   </div>
 </template>
