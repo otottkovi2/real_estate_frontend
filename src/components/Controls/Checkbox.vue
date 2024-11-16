bb2618e8829808d4f6a032d016d85cbf0d145cce
szt2otokov0 <szt2otokov@vasvari.org>
2024-11-16 16:26:12 +0100

    added checkboxes

---
+++ src/components/Controls/Checkbox.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  label: String,
+  id: {
+    type: String,
+    default() {
+      return '0'
+    },
+    validator(value, props) {
+      return value !== '0'
+    }
+  }
+})
+const label = props.label
+const checkId = props.id
+</script>
+
+<template>
+  <label :for="'check' + checkId">
+    {{label}}
+    <input type="checkbox" :id="'check' + checkId" @change="$emit('boxChecked','1')"
+    class="appearance-none relative size-5 border-2 border-gray-500 align-bottom
+     before:content-['×'] before:relative before:-top-3 before:-right-0.5 before:text-xs before:origin-bottom
+      before:align-[-5px] checked:before:text-2xl before:transition-all before:duration-100">
+  </label>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,14 @@ import TitleText from "@/components/Pre-styled/TitleText.vue";
 import SubTitleText from "@/components/Pre-styled/SubTitleText.vue";
 import Card from "@/components/Layout/Card.vue"
 import Button from "@/components/Controls/Button.vue"
+import Checkbox from "@/components/Controls/Checkbox.vue";
+import {ref} from "vue";
+
+let isBoxChecked = ref(false)
+function toggleBox(id)  {
+  if(id !== "1") return
+  isBoxChecked.value = !isBoxChecked.value
+}
 </script>
 
 <template>
@@ -16,7 +24,18 @@ import Button from "@/components/Controls/Button.vue"
         <Button name="Button" :onClick="()=>{}"></Button>
       </template>
     </Card>
-    <Card>2</Card>
+    <Card>
+      <template #card-header>
+        <SubTitleText>Checkbox</SubTitleText>
+      </template>
+      <template #default>
+        <Checkbox label="Check this:" id="1" @box-checked="toggleBox"></Checkbox>
+      </template>
+      <template #card-footer>
+        <div class="text-green-800" v-if="isBoxChecked">This box is checked!</div>
+        <div class="text-red-700" v-else>This box is unchecked!</div>
+      </template>
+    </Card>
     <Card>3</Card>
   </div>
 </template>
