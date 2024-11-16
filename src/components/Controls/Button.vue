137dfb4ba620e5f289727d5bf7c48d0dda103112
szt2otokov0 <szt2otokov@vasvari.org>
2024-11-16 14:14:45 +0100

    made Buttons

---
+++ src/components/Controls/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(["name","onClick"])
+const name = props.name
+const clickAction = props.onClick
+</script>
+
+<template>
+  <button @click="clickAction" class="bg-gray-300 focus:bg-gray-300 hover:bg-gray-500 active:bg-gray-600 p-2 m-2
+  rounded-md shadow-sm active:scale-90 active:transition active:duration-100 active:ease-in">{{name}}</button>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <script setup>
 import TitleText from "@/components/Pre-styled/TitleText.vue";
-import Card from "../components/Layout/Card.vue"
+import SubTitleText from "@/components/Pre-styled/SubTitleText.vue";
+import Card from "@/components/Layout/Card.vue"
+import Button from "@/components/Controls/Button.vue"
 </script>
 
 <template>
   <TitleText>Control demos</TitleText>
   <div class="grid grid-cols-3">
     <Card>1</Card>
+    <Card>
+        <Button name="Button" :onClick="()=>{}"></Button>
+    </Card>
     <Card>2</Card>
     <Card>3</Card>
   </div>
