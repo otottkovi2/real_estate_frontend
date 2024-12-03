083efe90e793ae08ad8eeccb21b416cf4e3ee955
szt2otokov0 <szt2otokov@vasvari.org>
2024-12-03 14:55:08 +0100

    started static site, basic site layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,59 +6,53 @@ import Card from "@/components/Layout/Card.vue"
 import Button from "@/components/Controls/Button.vue"
 import Checkbox from "@/components/Controls/Checkbox.vue";
 import ImagePicker from "@/components/Controls/ImagePicker.vue";
-
-let isBoxChecked = ref(false)
-const uploadedFiles = ref([])
-function toggleBox(id)  {
-  if(id !== "1") return
-  isBoxChecked.value = !isBoxChecked.value
-}
-
-function listUploadedFiles(paths) {
-  console.log(paths)
-  uploadedFiles.value = paths
-}
 </script>
 
 <template>
-  <TitleText>Control demos</TitleText>
-  <div class="grid grid-cols-3">
-    <Card>
-      <template #card-header>
-        <SubTitleText>Button</SubTitleText>
-      </template>
-      <template #default>
-        <Button name="Button" :onClick="()=>{}"></Button>
-      </template>
-    </Card>
-    <Card>
-      <template #card-header>
-        <SubTitleText>Checkbox</SubTitleText>
-      </template>
-      <template #default>
-        <Checkbox label="Check this:" id="1" @box-checked="toggleBox"></Checkbox>
-      </template>
-      <template #card-footer>
-        <div class="text-green-800" v-if="isBoxChecked">This box is checked!</div>
-        <div class="text-red-700" v-else>This box is unchecked!</div>
-      </template>
-    </Card>
-    <Card>
-      <template #card-header>
-        <SubTitleText>Image Picker</SubTitleText>
-      </template>
-      <template #default>
-        <ImagePicker :showPreviews="false" @file-uploaded="listUploadedFiles"></ImagePicker>
-      </template>
-      <template #card-footer>
-        <ul>
-          <li v-for="path in uploadedFiles">{{path}}</li>
-        </ul>
-      </template>
-    </Card>
-  </div>
+  <section class="min-h-96 border-2 border-gray-500 rounded-md flex justify-center items-center shadow-md
+  hover:shadow-inner">
+    <TitleText>Hero/Welcome image</TitleText>
+  </section>
+  <section>
+    <SubTitleText>description:</SubTitleText>
+    <p>Az ingatlan adásvétel életünk során az egyik legjelentősebb pénzügyi döntésünk egyike.</p>
+    <p>Mind eladói, mind vevői oldalról fontos, hogy zökkenőmentesen és minél kedvezőbb áron bonyolódjon le az ügylet.
+    </p>
+    <p>A legnagyobb precizitással, 15 éve ingatlan ügyeket képviselő ügyvéddel dolgozunk együtt, naprakészen tartva az
+      aktuális jogszabályi változásokat is.</p>
+    <p>Teljeskörű ügyintézést biztosítunk, megkímélve Önt az akár órákig tartó sorban állástól a hivatalos papírok
+      beszerzése-, vagy csak egy egyszerű tájékozódás miatt.</p>
+    <p>Például földhivatali-, építési-, térkép nyilvántartási és egyéb szükséges iratok.</p>
+    <p>Hitel, CSOK, valamint egyéb támogatások igénybe vételekor is teljes körű ügyintézéssel segítjük a sikeres
+      adásvételt.</p>
+    <p>Nincsenek rejtett költségek, mindenről pontos tájékoztatást adunk!</p>
+    <p>A mi ügyfeleink részére az adásvételi szerződés megkötéséhez szükséges tulajdoni lap INGYENES!</p>
+    <p>Lehetőség van vevői képviseletre is, amennyiben Ön máshol nézte ki álmai ingatlanát.</p>
+    <p>Amiben még segíteni tudunk:</p>
+    <ul>
+      <li>Energetikai tanúsítvány</li>
+      <li>Teljes körű hitelügyintézés<br/>(&nbsp;számla nyitás, személyi hitel, CSOK, egyéb támogatások&nbsp;)</li>
+      <li>Villamos hálózat felmérés, tanácsadás</li>
+      <li>Ingatlan átalakítás/kialakitás tanácsadás</li>
+      <li>Takarítási szolgáltatás</li>
+      <li>Lomtalanítás</li>
+      <li>Kisebb felújítási-, javítási munkálatok</li>
+    </ul>
+    <h5 class="text-lg font-semibold">Fontos Önnek a gyorsaság, precizitás, tapasztalat, korrektség?</h5>
+    <h3 class="text-xl font-semibold ml-2">Jó helyen van!</h3>
+  </section>
+  <section class="min-h-lvh border-2 border-gray-500 rounded-md flex">
+    <SubTitleText>House highlights go here</SubTitleText>
+  </section>
 </template>
 
 <style scoped>
+p,ul {
+  margin-bottom: 6pt;
+}
 
+li {
+  list-style: disc;
+  margin-left:3rem;
+}
 </style>
