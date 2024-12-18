416a66a4c7ce51e07bb53fcbb24a63263d3d4af0
szt2otokov0 <szt2otokov@vasvari.org>
2024-12-18 18:22:54 +0100

    undid my stupidity

--- src/components/Layout/HousePreview.vue
+++ src/components/Layout/HousePreview.vue
@@ -11,33 +11,20 @@ let location = props.location
 let price = props.price
 let area = props.area
 let roomCount = props.roomCount
-let displayTitle = ref(title)
-function resizeTitle() {
-  let bodyStyle = window.getComputedStyle(document.body)
-  let fontWidth = parseInt(bodyStyle.fontSize.replace('px',''))
-  let space = element.value.clientWidth
-  let fitLength = Math.round(space / fontWidth)-3
-  displayTitle.value = title.slice(0,fitLength-1)
-  if(displayTitle.value.length < title.length) displayTitle.value += "..."
-}
-onMounted(resizeTitle)
-window.addEventListener('resize',resizeTitle)
 </script>
 
 <template>
   <card>
     <template #default>
-      <div class="grid grid-cols-4 grid-rows-5 max-h-60 mr-2 pl-0 p-2" ref="preview-root">
-        <img :src="imgSrc" alt="előnézeti kép" class="col-start-1 col-end-1 row-start-1 row-span-5 max-h-full
-        justify-self-center self-center">
         <SubTitleText class="col-start-2 col-span-3 row-start-1" :title="title">{{ displayTitle }}</SubTitleText>
-        <i class="col-start-2 col-span-2 row-start-2 ml-2">{{ location }}</i>
-        <div class="col-start-2 row-start-3 row-span-2 text-2xl font-semibold ml-2 tracking-wide">{{ price }} Ft</div>
-        <hr class="border-2 border-secondary rounded-md col-start-2 col-span-3 row-start-4 ml-2 mb-2 self-end">
-        <div class="col-start-2 row-start-5 mr-3/12 text-right my-auto text-lg font-thin">{{ area }} m<sup>2</sup></div>
-        <div class="col-start-3 row-start-5 border-l-2 border-secondary rounded-md mx-auto"></div>
-        <div class="col-start-4 row-start-5 my-auto ml-3/12 text-left text-lg font-thin">{{ roomCount }} szoba</div>
+          {{title}}
       </div>
     </template>
   </card>
 </template>
+
+<style scoped>
+h2 {
+  @apply text-nowrap truncate text-ellipsis text-lg md:text-2xl
+}
+</style>
--- src/components/Pre-styled/SubTitleText.vue
+++ src/components/Pre-styled/SubTitleText.vue
@@ -1,3 +1,6 @@
+<script setup>
+</script>
+
 <template>
-<h2 class="font-bold text-2xl w-max text-center m-2"><slot></slot></h2>
+<h2 class="font-bold text-2xl text-balance m-2"><slot></slot></h2>
 </template>
